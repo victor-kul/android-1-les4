ffc6ff4880283aab9c4debcf4ff437b75b0de882
Victor-Kul <webdevelopervk@yandex.ru>
2016-05-12 23:18:11 +0500

    start-empty

---
+++ app/src/main/java/com/lessons/vi_key/android1_les2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.lessons.vi_key.android1_les2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
